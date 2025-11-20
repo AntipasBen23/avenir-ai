fa94348509bdadb325e412d6b8eb918b47106f58
Antipasben23 <benantipas@gmail.com>
2025-11-21 00:17:09 +0100

    added flash rays

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,24 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen relative overflow-hidden">
+      {/* First Diagonal Flash Effect */}
+      <div
+        className="absolute"
+        style={{
+          width: '128.64px',
+          height: '1218.61px',
+          top: '-214.92px',
+          left: '1300px',
+          transform: 'rotate(50.89deg)',
+          transformOrigin: 'top left',
+          background: 'linear-gradient(180deg, #0140AE 50%, rgba(115, 160, 240, 0) 100%)',
+          backdropFilter: 'blur(248.72720336914062px)',
+          opacity: 0.25,
+          pointerEvents: 'none'
+        }}
+      />
+      
       <Navbar />
       {/* Rest of your content will go here */}
     </div>
